3f4577fa903356df612ccef9f56bbf1c19b49efb
avichaich <avichay232@walla.co.il>
2013-04-30 17:18:54 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,96 @@
+<?php
+include('../header/login_header.php');
+include('../config/appConfig.php');
+?>
+ 
+<div id="slider">
+    <div align="center">
+        <h2>login</h2>
+    </div>
+        <div class="viewer">
+         
+            <div class="reel">
+                <div class="slide">
+                     
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="page">
+        <div id="content">
+            <div class="box1">
+                 
+     
+<table width="900" height="0" align="center">
+    <tr align="center">
+        <td  >
+        <h3><strong>הכנס את הפרטים הבאים:</strong></h3>
+         
+        </td>
+    </tr>
+</table>
+     
+         
+<table width="900" height="0" align="center" >
+    <tr>
+        <td>
+            <td align="center">
+                <form action="check.php" method="post">
+                <table border="0" align="center" bgcolor="#E0E0E0" width="400" >
+                    <tr>
+                        <td>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                            <td width="150" align="center" bgcolor="gray" style="color:#F8F8F8; font-size:20px;">שם משתמש:</td>
+                            <td width="150" align="center"><input type="text" name="userName" value="" /></td>
+                    </tr>
+                    <tr>
+                            <td width="150" align="center" bgcolor="gray" style="color:#F8F8F8; font-size:20px;">סיסמה</td>
+                            <td width="150" align="center"><input type="password" name="password" value="" /></td>
+                    </tr>
+                    <td><input type="submit" value="שלח" /></td>
+                     
+                </table>
+             
+             
+            <br>
+         
+             
+            </td>
+         
+             
+             
+             
+            </form>   
+        </td>
+    </tr>
+</table>
+                 
+                 
+                 
+                 
+                 
+                 
+            </div>
+             
+         
+             
+            <br class="clearfix" />
+        </div>
+        <div id="sidebar">
+         
+        </div>
+        <br class="clearfix" />
+    </div>
+ 
+</div>
+ 
+ 
+<div id="footer">
+    הזכויות שמורות לצוות 
+    control room
+</div>
+</body>
+</html>
